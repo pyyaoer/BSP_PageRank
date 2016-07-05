23635a40b9d659730fae36c533c9a8de96e6809b
pyyaoer <pyyaoer@gmail.com>
2016-07-05 23:21:04 +0800

    shrink sent data

--- bsp_dg.py
+++ bsp_dg.py
@@ -1,5 +1,4 @@
 import fileinput
-# return dict for id => (host, port)
 def divide_graph(filename, nodenum):
 	cnt = 0
 	from_to_dict = {}
@@ -49,25 +48,19 @@ def divide_graph(filename, nodenum):
 			f.close()
 			node_cnt = node_cnt + 1
 			f = open("task_"+str(node_cnt)+".txt", 'aw')
-
-	node_map = {}
 	for ele in from_to_dict:
 		if not node_dict.has_key(ele):
 			node_dict[ele] = nodenum
 			f.write(str(ele) + '\n')
-		node_src = node_dict[ele]
-		if not node_map.has_key(node_src):
-			node_map[node_src] = {}
-		for key in from_to_dict[ele]:
-			node_map[node_src][node_dict[key]] = None
 	f.close()
 
-	f = open("nodemap.txt", 'w')
-	for ele in node_map:
-		f.write(str(ele))
-		for key in node_map[ele]:
-			if key != ele:
-				f.write(' ' + str(key))
-		f.write('\n')
-	f.close()
+	node_adj = {}
+	for ele in to_from_dict:
+		nd = node_dict[ele]
+		if not node_adj.has_key(nd):
+			node_adj[nd] = {}
+		for key in to_from_dict[ele]:
+			node_adj[nd][key] = None
+
+	return node_adj
 
--- master.py
+++ master.py
@@ -3,6 +3,10 @@ import bsp_cm as cm
 import bsp_dg as dg
 import bsp_tr as tr
 
+node_list = {}
+node_list[1] = ("localhost", 8001)
+node_list[2] = ("localhost", 8002)
+
 port = 9000
 host = 'localhost'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,17 +14,23 @@ s.bind((host, port))
 s.listen(5)
 
 # divide the graph and generates the partition txt
-node_list = {}
-node_list[1] = ("localhost", 8001, False)
-dg.divide_graph("wiki-Vote.txt", 3)
+node_num = len(node_list)
+node_adj = dg.divide_graph("wiki-Vote.txt", node_num)
 
+# send partition infomation to slaves
 for key in node_list:
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.connect((node_list[key][0], node_list[key][1]))
-	tr.send_file(sock, 'hehe.txt', 0)
+	#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#sock.connect((node_list[key][0], node_list[key][1]))
+	#tr.send_file(sock, "task_"+str(key)+".txt", key)
+	print "task_"+str(key)+".txt"
 
 while True:
-	if cm.wait_slaves():
-		break
+	node_report_num = 0
+	while node_num != node_report_num:
+		#sock, addr = s.accept()
+		#node_id = int(tr.get_file(sock, "report_" + str(node_report_num+1) + ".txt"))
+		print "report_" + str(node_report_num+1) + ".txt"
+		node_report_num = node_report_num + 1
+	break
 
 s.close()
